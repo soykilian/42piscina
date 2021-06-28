3642bda9155c23c324ec7db4deadae595c968b36
Maria victoria Clerico da costa <mclerico@c3r13s6.42madrid.com>
2021-06-28 18:48:25 +0200

    12 parece guay

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -3,25 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsanchez <dsanchez@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: mclerico <mclerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 21:07:55 by mclerico          #+#    #+#             */
-/*   Updated: 2021/06/28 15:01:12 by dsanchez         ###   ########.fr       */
+/*   Updated: 2021/06/28 18:47:17 by mclerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-
-int	is_printable(char str)
-{
-	int	chartrue;
-
-	chartrue = 1;
-	if (str < 32 || str > 126)
-		chartrue = 0;
-	return (chartrue);
-}
 
 void	ft_char_to_hex(char src)
 {	
@@ -42,7 +32,7 @@ void	ft_char_to_hex(char src)
 		if (t < 10)
 			t = t + 48;
 		else
-			t = t + 55;
+			t = t + 87;
 		p[k--] = t;
 		dec = dec / 16;
 	}
@@ -56,13 +46,42 @@ void	print_str( char *src)
 	j = 0;
 	while (j < 16)
 	{
-		if (is_printable(src[j]))
+		if (src[j] > 31 && src[j] < 126)
 			write(1, &src[j], 1);
 		else
 			write(1, ".", 1);
 		j++;
 	}
 	write(1, "\n", 1);
+}
+
+void	dec_char_hex(unsigned long n)
+{
+	unsigned long		dec;
+	char				*pi;
+	char				a[16];
+	int					i;
+	int					t;
+
+	dec = 0;
+	i = 0;
+	t = 0;
+	while (i++ < 16)
+		a[i] = '0';
+	pi = a;
+	i = 15;
+	dec = n;
+	while (dec != 0)
+	{
+		t = dec % 16;
+		if (t < 10)
+			t = t + 48;
+		else
+			t = t + 87;
+		pi[i--] = t;
+		dec = dec / 16;
+	}
+	write(1, pi, 16);
 }
 
 void	ft_dec_to_hex(char *src, int *i)
@@ -90,16 +109,19 @@ void	ft_dec_to_hex(char *src, int *i)
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	int		*i;
-	char	*co;
-	int		inum;
+	int					*i;
+	char				*co;
+	int					inum;
+	long unsigned		dir;
 
 	co = addr;
 	inum = 0;
 	i = &inum;
 	while (*i < size)
 	{	
-		write(1, &co, 16);
+		dir = (long unsigned)co;
+		dec_char_hex(dir);
+		write(1, ": ", 2);
 		ft_dec_to_hex(co, i);
 		if (*i != size)
 			co += 16;
@@ -107,32 +129,13 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	return (addr);
 }
 
-void	ft_putnbr(long unsigned nbr)
-{
-	char	a;
-	if (nbr < 0)
-	{
-		write(1, "-", 1);
-		nbr =  nbr * -1;
-	}
-	if (nbr >= 10 )
-	{
-		ft_putnbr (nbr / 10);
-		ft_putnbr (nbr % 10);
-	}
-	else
-		a = nbr + '0';
-		write(1, &a, 1);
-}
-
 int	main(void)
 {
-	char	f[46] = "tu puta \nadre moulinette de los kojones basura";
+	char	f[16] = "Bonjour les amin";
 	char	*pi;
 	int		i;
-	long unsigned		a;
 
 	pi = f;
-	a = (long unsigned)pi;
-	ft_print_memory(pi, 46);
+	ft_print_memory(pi, 16);
+
 }
