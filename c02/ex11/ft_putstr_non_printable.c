7891102c3d64097a12c7311a4d92f2a75f768c8e
Maria victoria Clerico da costa <mclerico@c3r13s1.42madrid.com>
2021-06-29 14:23:19 +0200

    11 corregido

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,16 +6,17 @@
 /*   By: mclerico <mclerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 14:48:26 by mclerico          #+#    #+#             */
-/*   Updated: 2021/06/29 13:25:22 by mclerico         ###   ########.fr       */
+/*   Updated: 2021/06/29 14:21:09 by mclerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 void	ft_putchar(char c)
 {
 	    write(1, &c, 1);
 }
 
-int	is_printable(char str)
+int	is_printable(unsigned char str)
 {
 	int	chartrue;
 
@@ -25,13 +26,18 @@ int	is_printable(char str)
 	return (chartrue);
 }
 
-void	ft_dec_to_hex(int n, char *p)
+void	ft_dec_to_hex(unsigned char n)
 {
-	int		i;
+	char	a[2];
+	char	*pi;
 	int		t;
+	int		k;
 	int		dec;
 
-	i = 1;
+	a[0] = '0';
+	a[1] = '0';
+	pi = a;
+	k = 1;
 	dec = n;
 	while (dec != 0)
 	{
@@ -39,18 +45,19 @@ void	ft_dec_to_hex(int n, char *p)
 		if (t < 10)
 			t = t + 48;
 		else
-			t = t + 55;
-		p[i--] = t;
+			t = t + 87;
+		pi[k--] = t;
 		dec = dec / 16;
 	}
+	write(1, pi, 2);
 }
 
 void	ft_putstr_non_printable(char *str)
 {
-	int		i;
-	char	*p;
-	char	a[2];
-	char	*dest;
+	int				i;
+	unsigned char	*p;
+	unsigned char	a[2];
+	char			*dest;
 
 	dest = str;
 	i = 0;
@@ -61,10 +68,8 @@ void	ft_putstr_non_printable(char *str)
 	{
 		if (is_printable(str[i]) == 0)
 		{
-			ft_dec_to_hex(str[i], p);
 			ft_putchar('\\');
-			ft_putchar(p[0]);
-			ft_putchar(p[1]);
+			ft_dec_to_hex(str[i]);
 		}
 		else
 			ft_putchar(str[i]);
