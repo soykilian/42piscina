bb88cff5d73425db0016ef1d90a19c394060bb4b
Maria victoria Clerico da costa <mclerico@c3r8s1.42madrid.com>
2021-07-07 11:15:08 +0200

    te sigo dando queso?

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mclerico <mclerico@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 19:55:20 by mclerico          #+#    #+#             */
+/*   Updated: 2021/07/07 10:52:52 by mclerico         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+char	**sort_argv(char **argv, int n)
+{
+	int		i;
+	char	*aux;
+	int		ord;
+
+	i = 1;
+	ord = 0;
+	while (ord == 0)
+	{
+		while (i < n - 1)
+		{
+			ord = 1;
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				aux = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = aux;
+				ord = 0;
+			}
+			i++;
+		}
+		i = 0;
+	}
+	return (argv);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	argv = sort_argv(argv, argc);
+	while (i < argc)
+	{
+		while (argv[i][j] != 0)
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		j = 0;
+		i++;
+	}
+}
