80ed3fc78a5259dd471788e49b0048fa11d4e2b1
Diego Sanchez strange <dsanchez@c3r8s2.42madrid.com>
2021-07-13 15:05:02 +0200

    dale molinette mostrate un 100

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mclerico <mclerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 18:03:25 by mclerico          #+#    #+#             */
-/*   Updated: 2021/07/08 18:23:06 by mclerico         ###   ########.fr       */
+/*   Updated: 2021/07/13 13:53:34 by dsanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	ft_concatenate(char *p, char *pi, int index)
 {
 	int	i;
+
 	i = 0;
 	while (pi[i] != 0)
 	{
@@ -27,6 +28,7 @@ int	ft_concatenate(char *p, char *pi, int index)
 int	ft_strlen(char *str)
 {
 	int	j;
+
 	j = 0;
 	while (str[j] != 0)
 	{
@@ -39,7 +41,7 @@ void	ft_cat(char *p, int size, char **strs, char *sep)
 {
 	int		i;
 	int		c;
-	
+
 	i = 0;
 	c = 0;
 	while (i < size)
@@ -57,7 +59,7 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	int		i;
 	int		j;
 	int		c;
-	
+
 	p = NULL;
 	i = 0;
 	j = 0;
@@ -74,15 +76,4 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	i = 0;
 	ft_cat(p, size, strs, sep);
 	return (p);
-}
-
-int	main()
-{
-	char	**p;
-	p = malloc(8*2);
-	p[0] = "mundo";
-	p[1] = "hola";
-	printf("%s", ft_strjoin(2, p, "--"));
-	free(p);
-	return (0);
 }
--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -1,60 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_convert_base.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mclerico <mclerico@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/02 09:14:29 by mclerico          #+#    #+#             */
-/*   Updated: 2021/07/12 20:04:24 by mclerico         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int	check_base(char *base)
-{
-	int	l;
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	l = 0;
-	while (base[i++] != 0)
-		l++;
-	i = -1;
-	if (l < 2)
-		return (0);
-	while (i++ < l - 1)
-	{
-		if (base[i] == '+' || base[i] == '-')
-			return (0);
-		while (base[j] != 0)
-		{
-			if (base[i] == base[j] && i != j)
-				return (0);
-			j++;
-		}
-		j = 0;
-	}
-	return (1);
-}
-
-int	check_is_base(char str, char *base)
-{
-	int	i;
-
-	i = 0;
-	while (base[i] != 0)
-	{
-		if (str == base[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
+int		check_base(char *base);
+int		check_is_base(char str, char *base);
+void	ft_swap(char *sol);
+char	*ft_sign(char *res, int sign, int lres);
 
 int	ft_atoi(char *str, char *res, char *base)
 {
@@ -112,29 +62,6 @@ int	ft_x_10(char *res, char *base, int lenbase, int lres)
 	return (sol);
 }
 
-void	ft_swap(char *sol)
-{
-	int		i;
-	int		l;
-	int		end;
-	char	aux;
-
-	i = 0;
-	l = 0;
-	while (sol[l] != 0)
-		l++;
-	end = l / 2;
-	i = 0;
-	aux = 0;
-	while (i < end)
-	{
-		aux = sol[i];
-		sol[i] = sol[l - 1 - i];
-		sol[l - 1 - i] = aux;
-		i++;
-	}
-}
-
 char	*ft_10_y(int n, char *base, int l, int j)
 {
 	int			t;
@@ -151,62 +78,32 @@ char	*ft_10_y(int n, char *base, int l, int j)
 		sol[j] = base[t];
 	}
 	ft_swap(sol);
-	return (sol);	
-}
-char	*ft_sign(char *res, int sign, int lres)
-{
-	char	*sol;
-	int		i;
-	
-	i = 1;
-	sol = malloc(lres + 1);
-	if (sign == -1)
-	{
-		*sol = '-';
-		while ( i < lres)
-		{
-			sol[i] = res[i - 1];
-			i++;
-		}
-		return (sol);
-	}
-	return (res);
+	return (sol);
 }
 
 char 	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int		ls_to;
+	int		l[3];
 	int		i;
 	int		negative;
 	char	res[32];
-	int		ls;
-	int		ls_from;
 	int		dec;
-	int		lres;
 
 	i = 0;
-	ls = 0;
-	ls_from = 0;
-	ls_to = 0;
-	lres = 0;
+	l[0] = 0;
+	l[1] = 0;
+	l[2] = 0;
 	while (i < 32)
 		res[i++] = 0;
-	while (nbr[ls] != 0)
-		ls++;
 	if (!check_base(base_to) || !check_base(base_from))
 		return (0);
-	while (base_to[ls_to] != 0)
-		ls_to++;
-	while (base_from[ls_from++] != 0)
-		ls_from ++;
+	while (base_to[l[0]] != 0)
+		l[0]++;
+	while (base_from[l[1]++] != 0)
+		l[1]++;
 	negative = ft_atoi(nbr, res, base_from);
-	while (res[lres] != 0)
-		lres++;
-	dec = ft_x_10(res, base_from, ls_from, lres);
-	return(ft_sign(ft_10_y(dec, base_to, ls_to, 0), negative, lres));
-}
-
-int main (void)
-{
-	printf("%s", ft_convert_base("---bbacd", "abcd","champino"));
+	while (res[l[2]] != 0)
+		l[2]++;
+	dec = ft_x_10(res, base_from, l[1], l[2]);
+	return (ft_sign(ft_10_y(dec, base_to, l[0], 0), negative, l[2]));
 }
